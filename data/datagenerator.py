85b7741d9f8b6e1001c759c2a40613ff97611cfa
Abhijeet Avhale <111736148+AThreeH1@users.noreply.github.com>
2024-05-15 18:44:41 +0530

    Add files via upload

--- data/datagenerator.py
+++ data/datagenerator.py
@@ -1,117 +1,89 @@
+import os
+import sys
+
+# Get the directory of the current script
+current_dir = os.path.dirname(__file__)
+
+# Append the parent directory to the system path
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+sys.path.append(parent_dir)
+
+from utils.imports import *
+
+
 import random
 import numpy as np
 
-data = []
+def DataGenerate(net, sequence_length, dim):
 
-for r in range(1000):
+# l = length of signal
+# m = point selected at random at which the signal starts
+# d[] = dimensions for sinosuidal waves
+# sigs = signal dimension for waves to be classified
+# Cp = Coincedent signal point or number 
+# Ap = Any signal point or number
+# S1,2,3 = Signal Types
+# p or Tar = Singal selected or Target Signal respectively 
 
-    # initialising dimentions d1 and d2 with random numbers
-    d1 = []
-    d2 = []
+    dim = dim - 1
+    data = []
 
-    for i in range(100):
-        d1.append((random.random()) - 0.5)
-        d2.append((random.random()) - 0.5)
+    for r in range(net):
 
-    # initializing length of signal 'l' in dimension d1 & d2
-    l = (random.randint(10, len(d1)/2))
-    # print('length of main signal = ', l)
+        # initialising dimentions with random numbers
+        d = [[] for i in range(dim)]
+        sigs = []
 
-    # initializing the point 'm' from where the signal starts
-    m = (random.randint(0, (len(d1)-l)))
-    # print('The signal starts from index ', m)
+        for i in range(sequence_length):
+            for j in range(dim):
+                d[j].append((random.random()) - 0.5)
+            sigs.append((random.random()) - 0.5)
 
-    # # Initialising the three signals s1, s2 and s3 in dimension d2
-    # print('Signal s1 is triangular wave')
-    # print('Signal s2 is square wave')
-    # print('Signal s3 is downward semicircular wave')
+        def s1(t):
+            if t < 0.5:
+                return t
+            else:
+                return 1 - t
+        def s2(t):
+            if 0 <= t < 0.5:
+                return 0.5
+            else:
+                return -0.5
+        def s3(t):
+            return (-(0.5**2 - (t - 0.5)**2)**0.5)
 
-    # s1 - triangular wave
-    def s1(t):
-        if t < 0.5:
-            return t
-        else:
-            return 1 - t
+        Cp = random.randint(1, dim)
+        Ap = [i+1 for i in range(dim+1) if (i+1) != Cp]
+        l = random.randint(10, sequence_length // (dim + 1))
+        m = random.randint(0, (sequence_length // (dim + 1)) - l)
 
-    # s2 - square wave
-    def s2(t):
-        if 0 <= t < 0.5:
-            return 0.5
-        else:
-            return -0.5
+        for sn in range(dim+1):
 
-    # s3 - lower semicircle
-    def s3(t):
-        return (-(0.5**2 - (t - 0.5)**2)**0.5)
+            p = random.randint(1,3)
 
-    # selecting either of s1, s2 or s3 at random
-    p = random.randint(1,3)
-    # print('Target signal of s', p, ' is selected')
+            if (sn+1) == Cp:
+                Tar = p
+            for i in range(l):
+                for k in range(dim):
+                    if (sn+1) != Ap[k]:                    
+                        d[k][m] = (np.sin(i*2*np.pi/(l-1)))/2
 
-    # Inputting the signals in the respective dimensions
-    for i in range(l):
+                j = i/(l-1)
+                if p == 1:
+                    sigs[m] = s1(j)
+                if p == 2:
+                    sigs[m] = s2(j)
+                if p == 3:
+                    sigs[m] = s3(j)
 
-        d1[m] = (np.sin(i*2*np.pi/(l-1)))/2
+                m += 1
 
-        j = i/(l-1)
-        if p == 1:
-            d2[m] = s1(j)
-        if p == 2:
-            d2[m] = s2(j)
-        if p == 3:
-            d2[m] = s3(j)
+            l = (random.randint(10, sequence_length//(dim+1)))
+            m = random.randint(m, ((sn+2)*(sequence_length//(dim+1))-l))
 
-        m += 1
+        Batch = (*d[:dim], sigs, Tar-1)
+        data.append(Batch)
 
-    if len(d2) - m >= 10:
-
-        ps = random.randint(1,3)
-        # print('Post signal of s', ps, ' is selected')
-
-        Q = random.randint(10, min((len(d1) - m), 50))
-        # print('Post noise is of length = ', Q)
-
-        R = random.randint(0, (len(d1) - m - Q))
-
-        post = m + R
-        # print('Post noise starts from index ', post)
-
-        for i in range(Q):
-            j = i/(Q-1)
-            if ps == 1:
-                d2[post] = s1(j)
-            if ps == 2:
-                d2[post] = s2(j)
-            if ps == 3:
-                d2[post] = s3(j)
-
-            post += 1
-
-    if (m - l) >= 10:
-
-        bs = random.randint(1,3)
-        # print('Pre signal of s', bs, ' is selected')
-
-        Qn = random.randint(10, min((m - l), 50))
-        # print('Pre noise is of length = ', Qn)
-
-        Rn = random.randint(0, (m - l - Qn))
-
-        pre = Rn
-        # print('Pre noise starts from index ', pre)
-
-        for i in range(Qn):
-            j = i/(Qn-1)
-            if bs == 1:
-                d2[pre] = s1(j)
-            if bs == 2:
-                d2[pre] = s2(j)
-            if bs == 3:
-                d2[pre] = s3(j)
-
-            pre += 1
-
-    Batch = (d1, d2, p-1)
-    data.append(Batch)
-
-print('data stored')
+    print('data stored')
+    
+    return data
