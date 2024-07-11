a4bf9b19fbc877b04a664e6fe411ebbb170dadc4
AthreeH1 <ana.avhale@gmail.com>
2024-07-11 16:15:23 +0000

    more jumps in task 1b

--- data/datagenerator.py
+++ data/datagenerator.py
@@ -161,16 +161,16 @@ def SimpleDataGenerate(net, sequence_length):
 
 
 
-def task2(net, sequence_length):
+def task2(net, sequence_length, jumps):
 
 # l = length of signal
 # m = point selected at random at which the signal starts
 # d[] = list of dimensions for sinosuidal waves
 # sigs = list of signal-dimension for waves to be classified
-# Cp = Coincedent signal point or number 
+# Cp = Coincedent signal point or number
 # Ap = Any signal point or number
 # S1,2,3 = Signal Types
-# p or Tar = Singal selected or Target Signal respectively 
+# p or Tar = Singal selected or Target Signal respectively
 
     data = []
 
@@ -197,41 +197,43 @@ def task2(net, sequence_length):
         def s3(t):
             return (-(0.5**2 - (t - 0.5)**2)**0.5)
 
-        l = random.randint(10, sequence_length // 2)
+        l = random.randint(10, sequence_length // (jumps + 1) )
 
-        m1 = random.randint(0, (sequence_length // 2) - l)
-        m2 = random.randint(m1+l, ((sequence_length // 2) - l) + m1+l)
+        m = [random.randint(0, ((sequence_length // (jumps + 1)) - l))]
+        for i in range(jumps):
+            m.append(random.randint(m[-1]+l, (m[-1] + (sequence_length // (jumps + 1)))))
 
-        p1 = random.randint(1,3)
-        p2 = random.randint(1,3)
+        p = [random.randint(1,3) for _ in range(jumps+1)]
 
         for i in range(l):
-            d1[m1] = (np.sin(i*2*np.pi/(l-1)))/2
+            d1[m[0]] = (np.sin(i*2*np.pi/(l-1)))/2
 
             j = i/(l-1)
-            if p1 == 1:
-                d1[m2] = s1(j)
-                d2[m1] = s1(j)
-            if p1 == 2:
-                d1[m2] = s2(j)
-                d2[m1] = s2(j)
-            if p1 == 3:
-                d1[m2] = s3(j)
-                d2[m1] = s3(j)
+            for k in range(jumps):
+                if p[k] == 1:
+                    d1[m[k+1]] = s1(j)
+                    d2[m[k]] = s1(j)
+                if p[k] == 2:
+                    d1[m[k+1]] = s2(j)
+                    d2[m[k]] = s2(j)
+                if p[k] == 3:
+                    d1[m[k+1]] = s3(j)
+                    d2[m[k]] = s3(j)
+                
+                m[k] += 1
+            
+            if p[jumps] == 1:
+                d2[m[jumps]] = s1(j)
+            if p[jumps] == 2:
+                d2[m[jumps]] = s2(j)
+            if p[jumps] == 3:
+                d2[m[jumps]] = s3(j)
+            
+            m[jumps] += 1  
 
-            if p2 == 1:
-                d2[m2] = s1(j)
-            if p2 == 2:
-                d2[m2] = s2(j)
-            if p2 == 3:
-                d2[m2] = s3(j)
-
-            m1 += 1
-            m2 += 1
-
-        Batch = (d1, d2, p2-1)
+        Batch = (d1, d2, p[-1]-1)
         data.append(Batch)
 
     print('data stored')
-    
+
     return data
